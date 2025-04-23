38c2f2e8e7af81258bb7fff3dabb7e594aea053e
Estone <iestone@yeah.net>
2025-04-23 13:54:04 +0800

    Initial Commit

---
+++ PixelClock/PixelClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  PixelClockApp.swift
+//  PixelClock
+//
+//  Created by 何垒 on 2025/4/23.
+//
+
+import SwiftUI
+
+@main
+struct PixelClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PixelClockTests/PixelClockTests.swift
@@ -0,0 +1,17 @@
+//
+//  PixelClockTests.swift
+//  PixelClockTests
+//
+//  Created by 何垒 on 2025/4/23.
+//
+
+import Testing
+@testable import PixelClock
+
+struct PixelClockTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
