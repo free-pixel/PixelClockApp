c375c969f888fbb2c8cf19ca4a770c2c7ec669fc
Estone <chinaestone@gmail.com>
2026-01-09 23:45:41 +0800

    Revert "Merge pull request #7 from free-pixel/feat/safety-coding"
    This reverts commit 1f65d976cf7f5ab5c2b2177ccc27792b6f0d48dc.

--- PixelClockTests/PixelClockTests.swift
+++ PixelClockTests/PixelClockTests.swift
@@ -10,53 +10,8 @@ import Testing
 
 struct PixelClockTests {
 
-    @Test func testTimerViewModelInitialization() async throws {
-        let viewModel = TimerViewModel()
-        
-        #expect(viewModel.timerValue == 25 * 60)
-        #expect(viewModel.currentState == .task)
-        #expect(viewModel.timerRunning == false)
-        #expect(viewModel.completedTasks == 0)
-    }
-
-    @Test func testSwitchToTaskMode() async throws {
-        let viewModel = TimerViewModel()
-        viewModel.switchTo(.task)
-        
-        #expect(viewModel.timerValue == 25 * 60)
-        #expect(viewModel.currentState == .task)
-    }
-
-    @Test func testSwitchToBreakMode() async throws {
-        let viewModel = TimerViewModel()
-        viewModel.switchTo(.break)
-        
-        #expect(viewModel.timerValue == 5 * 60)
-        #expect(viewModel.currentState == .break)
-    }
-
-    @Test func testSwitchToLongBreakMode() async throws {
-        let viewModel = TimerViewModel()
-        viewModel.switchTo(.longBreak)
-        
-        #expect(viewModel.timerValue == 15 * 60)
-        #expect(viewModel.currentState == .longBreak)
-    }
-
-    @Test func testSwitchMultipleModes() async throws {
-        let viewModel = TimerViewModel()
-        
-        viewModel.switchTo(.task)
-        #expect(viewModel.timerValue == 25 * 60)
-        
-        viewModel.switchTo(.break)
-        #expect(viewModel.timerValue == 5 * 60)
-        
-        viewModel.switchTo(.longBreak)
-        #expect(viewModel.timerValue == 15 * 60)
-        
-        viewModel.switchTo(.task)
-        #expect(viewModel.timerValue == 25 * 60)
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
     }
 
 }
